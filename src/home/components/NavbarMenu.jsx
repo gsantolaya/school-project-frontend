2365df25bb9e999ceb689f9959f367e14dc85fd7
Guillermo Santolaya <guillermosantolaya.93@gmail.com>
2023-05-13 20:41:36 -0300

    navbar terminado, creando sideMenu y footer

---
+++ src/home/components/NavbarMenu.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import './NavbarMenu.css';
+
+function NavbarMenu() {
+  return (
+    <Navbar className="navContainer" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="home">LOGO</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link className="navLinks" href="home">Inicio</Nav.Link>
+            <Nav.Link className="navLinks" href="aboutUs">Acerca de nosotros</Nav.Link>
+            <Nav.Link className="navLinks" href="login">Login</Nav.Link>
+            <Nav.Link className="navLinks" href="register">Registro</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarMenu;
---
+++ src/home/pages/AboutUsScreen.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AboutUsScreen = () => {
+  return (
+    <div>AboutUsScreen</div>
+  )
+}
