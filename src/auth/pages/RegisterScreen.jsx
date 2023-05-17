d830a1d83f403d84800341549b590a2aff63d4a5
Masiel <masiel31@msn.com>
2023-05-17 11:54:31 -0300

    auth-login-register

--- src/auth/pages/RegisterScreen.jsx
+++ src/auth/pages/RegisterScreen.jsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import "./Auth.css"
+
 
 export const RegisterScreen = () => {
+  const navigate = useNavigate()
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const Submit = data => {
+    navigate("/home")
+    console.log(data)};
+
   return (
-    <div>Register</div>
+    <div className="d-flex  justify-content-center align-items-center flex-column mt-5">
+      <h1 className="title" >Registrate</h1>
+    <div  >
+
+      <Form onSubmit={handleSubmit(Submit)}>
+     <Form.Group className="mb-3 "  controlId="formBasicEmail">
+        <Form.Label>Nombre y Apellido</Form.Label>
+        <Form.Control 
+         placeholder="Carlos Perez" 
+        type="text" 
+        id="user"
+        name="user"
+        {...register("user",{ required: true })}
+        />
+          {errors?.user && <span className="text-danger">Este campo es requerido</span>}
+      </Form.Group>
+
+      <Form.Group className="mb-3 "  controlId="formBasicEmail">
+        <Form.Label>Correo Electronico</Form.Label>
+        <Form.Control 
+          placeholder="carlos@gmail.com"   
+        type="email" 
+        id="email"
+        name="email"
+        {...register("email",{ required: true })}/>
+        {errors?.email && <span className="text-danger">Este campo es requerido</span>}
+        
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label >Contraseña</Form.Label>
+        <Form.Control 
+         placeholder="*******"
+         type="password" 
+         id="password"
+         name="password"
+         {...register("password",{ required: true , pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/})}
+        />
+        {errors?.password && <span className="text-danger">Este campo es requerido. </span>}
+         {errors?.password && errors.password.type === "pattern" && <span className="text-danger">La contraseña debe tener al menos 6 caracteres, una mayuscula y un numero</span>}
+      </Form.Group>
+      
+      
+  
+      <div className="justify-content-center  d-flex">
+      <Button variant="outline-secondary" type="submit" className="w-50" >
+        Acceder
+      </Button>
+      </div>
+
+    </Form>
+<div className="flex-column d-flex align-items-center mt-2" >
+    <Form.Text>
+    ¿Tienes una cuenta?
+      </Form.Text>  
+      
+<Link  to={"/login"} >Ingresa Aquí</Link>
+</div>
+
+    </div>
+    </div>
   )
 }
--- src/home/pages/AdministrativeStaffScreen.jsx
+++ src/home/pages/AdministrativeStaffScreen.jsx
@@ -1,7 +1,66 @@
 import React from 'react'
+import { SideMenu } from '../components/SideMenu'
+import Table from 'react-bootstrap/Table';
+import Pagination from 'react-bootstrap/Pagination';
+
+let active = 1;
+let items = [];
+for (let number = 1; number <= 10; number++) {
+  items.push(
+    <Pagination.Item key={number} active={number === active}>
+      {number}
+    </Pagination.Item>,
+  );
+}
 
 export const AdministrativeStaffScreen = () => {
   return (
-    <div>AdministrativeStaffScreen</div>
+    <>
+    <SideMenu />
+    <div  className='Container col-md-9 col-lg-10 position-absolute end-0 p-5 text-center'>
+<h1>Personal Administrativo</h1>
+
+<Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Apellido</th>
+          <th>Nombre</th>
+          <th>Fecha Ingreso</th>
+          <th>Telefono</th>
+          <th>Email</th>
+          <th>Direccion</th>
+          <th>Nombre Institucion</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+      
+      </tbody>
+    </Table>
+    <div className='justify-content-center align-items-center d-flex mt-3'>
+    <Pagination size="sm">{items}</Pagination>
+    </div>
+    </div>
+    </>
   )
 }
