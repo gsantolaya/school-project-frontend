efd110703134f43ea87485189a3d690a5e4aa79b
Masiel <masiel31@msn.com>
2023-05-25 00:31:55 -0300

    Error404 page is created.

--- src/Error404Screen.jsx
+++ src/Error404Screen.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-import './Error404Screen.css';
+import React from "react";
+import Error404 from "./Error404.png";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Figure from "react-bootstrap/Figure";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+
 
 
 export const Error404Screen = () => {
   return (
-    <div>Error404Screen</div>
-  )
-}
+    <Container fluid style={{ background: " #7A0045" }}>
+      <Row>
+        <Col className="text-center">
+          <Figure>
+            <Figure.Image
+              style={{ width: "100" }}
+              alt="Error 404"
+              src={Error404}
+            />
+            <Button variant="dark" className="w-50 mt-3"
+              style={{
+                borderRadius: "20px",
+              }}
+            >
+              <Link
+                className="fs-3 fw-ligh    "
+                to={"/home"}
+                style={{
+                  textDecoration: "none",
+                  color: " #C85D16",
+                  cursor: "pointer",
+                }}
+              >
+                Volver Al Inicio
+              </Link>
+            </Button>
+          </Figure>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
